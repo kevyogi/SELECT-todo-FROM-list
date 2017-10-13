5afe221482a061b7ba24c2d93565293be0b86ef1
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-10-12 15:19:35 -1000

    added column completed_at

--- todo_app.sql
+++ todo_app.sql
@@ -1,10 +1,12 @@
--- DROP DATABASE IF EXISTS todo_app;
+DROP DATABASE IF EXISTS todo_app;
 
--- DROP USER IF EXISTS michael;
+DROP USER IF EXISTS michael;
 
--- CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
+CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
 
--- CREATE DATABASE todo_app;
+CREATE DATABASE todo_app;
+
+\c todo_app;
 
 CREATE TABLE tasks (
   id            SERIAL PRIMARY KEY NOT NULL,
@@ -13,5 +15,11 @@ CREATE TABLE tasks (
   created_at    timestamp    NOT NULL DEFAULT now(),
   updated_at    timestamp    NULL,
   completed     boolean      NOT NULL DEFAULT false
-  )
+);
+
+ALTER TABLE tasks
+  DROP COLUMN IF EXISTS completed;
+
+ALTER TABLE tasks
+  ADD COLUMN completed_at timestamp NULL DEFAULT NULL;
 
