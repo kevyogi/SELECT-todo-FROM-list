15e48ea3ec4a2bfc5dd7f157ba98e9f8d4159e01
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-10-12 16:13:12 -1000

    sorted in desc order

--- todo_app.sql
+++ todo_app.sql
@@ -1,13 +1,14 @@
+-- 1
 DROP DATABASE IF EXISTS todo_app;
-
+-- 2
 DROP USER IF EXISTS michael;
-
+-- 3
 CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
-
+-- 4
 CREATE DATABASE todo_app;
-
+-- 5
 \c todo_app;
-
+-- 6, 7
 CREATE TABLE tasks (
   id            SERIAL PRIMARY KEY NOT NULL,
   title         varchar(255)       NOT NULL,
@@ -16,36 +17,41 @@ CREATE TABLE tasks (
   updated_at    timestamp          NULL,
   completed     boolean            NOT NULL   DEFAULT false
 );
-
+-- 9
 ALTER TABLE tasks
   DROP COLUMN IF EXISTS completed;
-
+-- 10
 ALTER TABLE tasks
   ADD COLUMN completed_at timestamp NULL DEFAULT NULL;
-
+-- 11
 ALTER TABLE tasks
   ALTER COLUMN updated_at SET NOT NULL;
 
 ALTER TABLE tasks
   ALTER COLUMN updated_at SET DEFAULT now();
-
+-- 12
 INSERT INTO tasks (title, description, created_at, updated_at, completed_at)
   VALUES ('Study SQL', 'Complete this exercise', now(), now(), NULL);
-
+-- 13
 INSERT INTO tasks (title, description)
   VALUES ('Study PostgreSQL', 'Read all the documentation');
-
-SELECT *
+-- 14
+SELECT title
 FROM tasks
 WHERE completed_at IS NULL;
-
+--15
 UPDATE tasks
 SET completed_at = now()
 WHERE id = 1;
-
+-- 16
 SELECT title, description
 FROM tasks
 WHERE completed_at IS NULL;
+-- 17
+SELECT *
+FROM tasks
+ORDER BY created_at DESC;
+--18
 
 -- SELECT *
 -- FROM tasks;
