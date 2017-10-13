81540b979702066ce12b3b736c387a52bd4bc860
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-10-12 15:41:16 -1000

    did two insertions

--- todo_app.sql
+++ todo_app.sql
@@ -10,11 +10,11 @@ CREATE DATABASE todo_app;
 
 CREATE TABLE tasks (
   id            SERIAL PRIMARY KEY NOT NULL,
-  title         varchar(255) NOT NULL,
-  description   text         NULL,
-  created_at    timestamp    NOT NULL DEFAULT now(),
-  updated_at    timestamp    NULL,
-  completed     boolean      NOT NULL DEFAULT false
+  title         varchar(255)       NOT NULL,
+  description   text               NULL,
+  created_at    timestamp          NOT NULL   DEFAULT now(),
+  updated_at    timestamp          NULL,
+  completed     boolean            NOT NULL   DEFAULT false
 );
 
 ALTER TABLE tasks
@@ -28,3 +28,9 @@ ALTER TABLE tasks
 
 ALTER TABLE tasks
   ALTER COLUMN updated_at SET DEFAULT now();
+
+INSERT INTO tasks (title, description, created_at, updated_at, completed_at)
+  VALUES ('Study SQL', 'Complete this exercise', now(), now(), NULL);
+
+INSERT INTO tasks (title, description)
+  VALUES ('Study PostgreSQL', 'Read all the documentation');
