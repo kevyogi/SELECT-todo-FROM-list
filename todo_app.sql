d6255cad4a96cff2a4335b25ab4b4725d4d2eda1
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-10-12 14:56:14 -1000

    made table

--- todo_app.sql
+++ todo_app.sql
@@ -1,7 +1,16 @@
-DROP DATABASE IF EXISTS todo_app;
+-- DROP DATABASE IF EXISTS todo_app;
 
-DROP USER IF EXISTS michael;
+-- DROP USER IF EXISTS michael;
 
-CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
+-- CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
 
-CREATE DATABASE todo_app;
+-- CREATE DATABASE todo_app;
+
+CREATE TABLE tasks (
+  id            SERIAL PRIMARY KEY NOT NULL,
+  title         varchar(255) NOT NULL,
+  description   text         NULL,
+  created_at    timestamp    NOT NULL DEFAULT now(),
+  updated_at    timestamp    NULL,
+  completed     boolean      NOT NULL DEFAULT false
+  )
